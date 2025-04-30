e2c08bb4f540abec94c90dae36c49ac8d1a52c2d
Yisongjing <24aw0105@jec.ac.jp>
2025-04-30 16:48:44 +0900

    文字重叠部分效果不生效

--- src/page/Top.jsx
+++ src/page/Top.jsx
@@ -1,22 +1,50 @@
-import React, { useEffect, useState, useRef } from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
 import VideoPlayer from "../components/VideoPlayer";
 
-
-
 function Top() {
+    const videoRef = useRef(null);
+    const textRef = useRef(null);
+
+    // 设置滚动动画
+    useEffect(() => {
+        const scrollText = () => {
+            if (textRef.current) {
+                // 重置位置到起始点
+                if (textRef.current.scrollLeft >= (textRef.current.scrollWidth - textRef.current.clientWidth)) {
+                    textRef.current.scrollLeft = 0;
+                } else {
+                    // 缓慢滚动
+                    textRef.current.scrollLeft += 1;
+                }
+            }
+        };
+        
+        // 设置滚动定时器
+        const scrollInterval = setInterval(scrollText, 30);
+        
+        return () => {
+            clearInterval(scrollInterval);
+        };
+    }, []);
     
-return(
-    <>
-        <div className="ttl">TECHNOLOGY ART MEDIA</div>
+    return (
+      <div className="mv">
+        {/* 视频背景 */}
         <VideoPlayer
-            src="https://www.bilibili.com/opus/412118424337929280"
-            // title="SOMIN 宣传片"
-            // description="这是我们最新的品牌视频"
-            poster="https://your-oss/cover.jpg"
+          ref={videoRef}
+          src="/sample.MOV"
+          poster="/sample.jpg"
+          className="video-player"
         />
-    </>
-)
+
+        {/* 滚动文字容器 */}
+        <div className="scrolling-text-wrapper" ref={textRef}>
+          <div className="scrolling-text ttl">
+            TECHNOLOGY ART MEDIA TECHNOLOGY ART MEDIA TECHNOLOGY ART MEDIA TECHNOLOGY ART MEDIA
+          </div>
+        </div>
+      </div>
+    );
 }
 
-export default Top
+export default Top;
